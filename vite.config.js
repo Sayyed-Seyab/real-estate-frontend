c8f30c78e5faf00ab5c00af15f0a72fe9a83d3ff
Sayyed-Seyab <syed.seyab767@gmail.com>
2025-03-28 10:10:04 +0500

    build

--- vite.config.js
+++ vite.config.js
@@ -9,4 +9,12 @@ export default defineConfig({
   optimizeDeps: {
     include: ["jodit-react"],
   },
+  build: {
+    outDir: "dist", // Ensure Vite outputs build files to 'dist'
+    emptyOutDir: true, // Clears the output directory before building
+  },
+  server: {
+    port: 3000, // Optional: Change to your preferred port
+    option:true,
+  },
 });
